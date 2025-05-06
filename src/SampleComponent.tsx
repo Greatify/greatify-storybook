b39ed4690366747817cce59d12adb34fc203d37e
Ayush Chittranshi <ayush.c@greatify.ai>
2025-05-06 16:07:17 +0530

    Added latest config

--- src/SampleComponent.tsx
+++ src/SampleComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 const ExampleComponent: React.FC = () => {
-  return <div>Hello, Component Library!</div>;
+  return <div>Hello, Component Library from Ayush!</div>;
 };
 export default ExampleComponent;
