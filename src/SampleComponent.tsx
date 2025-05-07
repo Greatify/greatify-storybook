7830686f6e5accd820fb55922eed2414ec11dd2b
Ayush Chittranshi <ayush.c@greatify.ai>
2025-05-07 18:07:34 +0530

    Added storybook changes and tailwind

--- src/SampleComponent.tsx
+++ src/SampleComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 const ExampleComponent: React.FC = () => {
-  return <div>Hello, Component Library from Ayush!</div>;
+  return <div className='bg-blue-500'>Hello, Component Library from Ayush!</div>;
 };
 export default ExampleComponent;
